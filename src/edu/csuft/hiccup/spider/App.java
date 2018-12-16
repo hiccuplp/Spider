ff652c132460addd53ecf014f344fa8dd2173a44
hiccuplp <40220939+hiccuplp@users.noreply.github.com>
2018-12-16 17:21:42 +0800

    解析影片的信息

--- src/edu/csuft/hiccup/spider/App.java
+++ src/edu/csuft/hiccup/spider/App.java
@@ -1,9 +1,61 @@
 package edu.csuft.hiccup.spider;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class App {
 	/*
 	 *      @author  weitao
 	 * 
 	 */
+	// alt* /
+	public static void main(String[] args) {
+	
+		//  目标 URL
+		String url="https://movie.douban.com/top250";
+		
+		//使用  jsoup 抓取文档
+		try {
+			Document doc =  Jsoup.connect(url).get();
+			
+////			String title = doc.title();
+//			String data = doc.data();
+//			
+//			
+//			System.out.println(title);
+//			System.out.println(data);
+//			
+			//Elements es = doc.select("a");
+			//System.out.println(es.size());
+			
+		    Elements es = doc.select(".grid_view .item");
+		    System.out.println(es.size());
+		    
+		    //CH创建一个存储影片的列表
+		    ArrayList<film> list=new ArrayList<>();
+		    
+		    for(Element e :es){   
+		    	film f = new film();
+		    	// eiy每一部影片
+		    	Element t = e.select(".title").first();
+		    	String num=e.select(".star span").last().text();
+		    	System.out.println(t.text()+","+ num.substring(0,num.length()-3));
+		    	
+//		    	f.id;
+//		    	f.title;
+		    	list.add(f);
+		    }
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+}
 
 }
---
+++ src/edu/csuft/hiccup/spider/film.java
@@ -0,0 +1,39 @@
+package edu.csuft.hiccup.spider;
+
+public class film {
+
+	/*
+	 * 影片名称
+	 */
+	String title;
+	
+	/*
+	 * 相关信息
+	 */
+	String info;
+	
+	/*
+	 * 评分和排名
+	 */
+	double rating ;
+	
+	/*
+	 * 评分人数
+	 */
+	int num;
+	
+	/*
+	 *排名
+	 */
+	int id;
+	
+	/*
+	 * 海报
+	 */
+	String  poster;
+	
+	/*
+	 * 短评
+	 */
+	String quote;
+}
